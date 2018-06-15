479fb5a63223c434ddc5d9eaede9df3425e8e1a9
Jubileu99 <victor_luizbernardes5@outlook.com>
2018-06-15 00:49:36 -0300

    arrumar o 09.c

--- 06.c
+++ 06.c
@@ -6,29 +6,60 @@
     |  Data :  09/06/2018            |
 */
 
-#define t 7
+#define tam 7
 
-int main(){
+int main()
+{
 
-    int vetor[t] = {1,3,4,6,8,9,11},
-    aux,
-    x;
+    int vetor[tam] = {1, 3, 4, 6, 8, 9, 11},
+        aux,
+        x;
 
     printf("Informe o valor a ser buscado\n");
-    scanf("%i",&x);
+    scanf("%i", &x);
 
-    
-    if( vetor[(t-1)/2] == x ){
-        printf("Elemento %i achado no vetor\n",x);
-    }else if(x > vetor[(t-1)/2] ){
-        for(int i=(t+1)/2 ; i < t ; i++){
-            if( x == vetor[t-i]){
-                printf("Elemento %i achado no vetor\n",x);
-            }
-        }
-    }else if(x < vetor[(t-1)/2] ){
-
+    if (vetor[(tam - 1) / 2] == x)
+    {
+        printf("Elemento %i achado no vetor\n", x);
     }
-    
+    else if (x > vetor[(tam - 1) / 2])
+    {
+        if (x == vetor[tam - 2])
+        {
+            printf("Elemento %i achado no vetor\n", x);
+        }
+        else if (x == vetor[tam - 1])
+        {
+            printf("Elemento %i achado no vetor\n", x);
+        }
+        else if (x == vetor[tam - 3])
+        {
+            printf("Elemento %i achado no vetor\n", x);
+        }
+        else
+        {
+            printf("Elemento %i nao encontrado no vetor\n", x);
+        }
+    }
+    else if (x < vetor[(tam - 1) / 2])
+    {
+        if (x == vetor[1])
+        {
+            printf("Elemento %i achado no vetor\n", x);
+        }
+        else if (x == vetor[2])
+        {
+            printf("Elemento %i achado no vetor\n", x);
+        }
+        else if (x == vetor[0])
+        {
+            printf("Elemento %i achado no vetor\n", x);
+        }
+        else
+        {
+            printf("Elemento %i nao encontrado no vetor\n", x);
+        }
+    }
+
     return 0;
 }
--- 09.c
+++ 09.c
@@ -8,30 +8,57 @@
 
 #define tam 10
 
+int verif_zero(int *vetor){
+
+    int cont = 0;
+
+    for (int i = 0; i < tam; i++)
+        if (vetor[i] == 0)
+            cont++;
+
+    return cont;
+}
+
 int main(){
 
-    int vetor[tam] = {6,4,8,0,4,0,5,6,9,4},
-    aux,
-    cont=0;
+    
+    int vetor[tam] = {5,4,8,0,4,9,0,6,9,4},
+    aux[tam],
+    contZ,
+    x=0;
 
-    for(int i = 0; i < tam ; i++){
-        if (vetor[i] == 0)
-        {
-            cont++;
-            
-        }
-    }
+    contZ = verif_zero(vetor);
 
-    for(int i = 0 ; i < tam-1 ; i++){
+    /*
+    for(int i = 0 ; i < tam-cont ; i++){
         if(vetor[i] == 0){
-            aux = vetor[i+1];
-            vetor[i+1] = 0;
-            vetor[i] = aux;
+            cont1++;
+            aux2[i] = vetor[i+1];
+            aux2[tam-cont1] = 0;
+        }else{
+            aux2[i] = vetor[i];
+        }
+    }
+    */
+
+    
+    for(int i = 0; i < tam; i++){
+        if(vetor[i] != 0 ){
+            aux[x] = vetor[i];
+            x++;
         }
     }
 
+    
+    while(tam-1 <= contZ){
+        
+    }
+    
+
+    
+
     for(int i = 0; i < tam ; i++){
-        printf("%i\n",vetor[i]);
+        printf("%i\n",aux[i]);
     }
 
     return 0;
